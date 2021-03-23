048bc917b556a4678b19afff64c766dd4b8702e7
sharklu42 <81252400+sharklu42@users.noreply.github.com>
2021-03-23 21:21:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import undetected_chromedriver as uc
 from csv import DictWriter
+import os
 
 options = uc.ChromeOptions()
 
+options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
-driver = uc.Chrome(options=options)
+driver = uc.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=options)
 
 
 def get_url(k, c):
