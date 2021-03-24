26df3a7286c18b0d3ca48e29572e002a492a8bba
sharklu42 <81252400+sharklu42@users.noreply.github.com>
2021-03-24 18:42:06 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template, request, send_file, after_this_request
 import main
 import os
 
+
 app = Flask(__name__)
 
-#rendering deafoult flask template
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-#getting data from the input fields
+
 @app.route('/get-data', methods=['POST'])
 def my_form_post():
     keyword = request.form['keyword']
@@ -18,7 +19,7 @@ def my_form_post():
     app_id = request.form['app_id']
     main.run_script({"keyword": keyword, "country": country, "locale": locale, "app_id": app_id})
     file_path = "./output.csv"
-#saving file into a memory to remove after downloding. Workaround for windows only
+
     def generate():
         with open(file_path) as f:
             yield from f
--- main.py
+++ main.py
@@ -3,53 +3,55 @@ from csv import DictWriter
 from selenium import webdriver
 import os
 
-
-#initializing chrom options
 options = webdriver.ChromeOptions()
 
-
-#binary location is set to run script on Heroku server
-#change it accordingly to the requirements of your own or remove to run on local
 options.binary_location = os.environ.get('GOOGLE_CHROME_SHIM', None)
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-
-#chrome executable path was set to run script on Heroku server
-#change it accordingly to the requirements of your own or remove to run on local
 driver = webdriver.Chrome(executable_path="chromedriver", options=options)
 
 
 def get_url(k, l, c):
     return f"https://play.google.com/store/search?q={k}&c=apps&hl={l}&gl={c}"
 
-#openeing headless chrome for scraping in background
+
 def get_headless_window(url):
     driver.get(url=url)
 
-    
+
+# parametirise url +
+# find app id # in a list +
+# create a simple webform +
+# connect webform with script +
+# put output in a csv and download it after click on button +
+# remove file after downloading +
+# fix bugs +
+# remove debug+
+# switch parameter from language to country
+# add input field for language
+
+
 def get_web_elements(xpath):
     return driver.find_elements_by_xpath(xpath)
 
-#search for a keyword in a list of first 50 apps
+
 def find_app_id_index(app_id):
     app_id_xpath = "/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/c-wiz/c-wiz/c-wiz/div/div[2]/div"
     list_of_apps = get_web_elements(app_id_xpath)
     app_id_list = []
     for app in list_of_apps:
         app_id_list.append(app.find_element_by_tag_name("a").get_attribute("href"))
- #try block is searching for an app_id in a list, it returns index number if there is one in the first 50 list    
     try:
         i = app_id_list.index(f"https://play.google.com/store/apps/details?id={app_id}")
     except ValueError:
- #returning message if app id is not in the first 50 list
         i = "app is not in the first 50th list"
         return i
     else:
         return i + 1
 
-#creating a new csv file to store output
+
 def add_into_csv(input_data):
     fieldnames = ["KEYWORD", "INDEX"]
     with open("output.csv", 'a') as o:
@@ -57,14 +59,13 @@ def add_into_csv(input_data):
         writer_object.writerow(input_data)
         o.close()
 
-        
+
 def prepare_keyword(keyword):
     k1 = keyword.replace('\r\n', ",")
     new_keyword = k1.split(",")
     return new_keyword
 
-        
-#Main script runner
+
 def run_script(data):
     keywords = prepare_keyword(data['keyword'])
     for key in keywords:
